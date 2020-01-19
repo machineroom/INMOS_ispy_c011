38cff45951110e347eb044e74188093e8df6bb17
James Wilson <macihenroomfiddling@gmail.com>
2020-01-19 17:46:31 +0000

    Test c011/gpio optimisations. not much difference

--- c011.c
+++ c011.c
@@ -3,11 +3,16 @@
 #include "pins.h"
 #include "c011.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
-#define TCSLCSH (60)
-#define TCSHCSL (50)
-#define TCSLDrV (50)
+#define TCSLCSH (6)
+#define TCSHCSL (5)
+#define TCSLDrV (5)
+
+static uint8_t read_c011(void);
+
+static uint32_t *gpio_base;
 
 static void sleep_ns(int ns) {
     struct timespec s = {0,ns};
@@ -26,31 +31,46 @@ static void set_control_output_pins(void) {
 }
 
 static void set_data_output_pins(void) {
-    bcm2835_gpio_fsel(D0, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D1, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D2, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D3, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D4, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D5, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D6, BCM2835_GPIO_FSEL_OUTP);
-    bcm2835_gpio_fsel(D7, BCM2835_GPIO_FSEL_OUTP);
+    // SEL0 pins 0-9 output
+    // pin rr  9  8  7  6  5  4  3  2  1  0
+    //     00001001001001001001001001001001
+    //        0   9   2   4   9   2   4   9
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL0/4, 0x09249249UL);
+    // SEL1 pins 10-19 output
+    // pin rr 19 18 17 16 15 14 13 12 11 10
+    //     00001001001001001001001001001001
+    //        0   9   2   4   9   2   4   9
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL1/4, 0x09249249UL);    
+    // SEL2 pins 20-29 output       
+    // pin rr 29 28 27 26 25 24 23 22 21 20
+    //     00001001001001001001001001001001
+    //        0   9   2   4   9   2   4   9
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL2/4, 0x09249249UL);    
 }
 
 static void set_data_input_pins(void) {
-    bcm2835_gpio_fsel(D0, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D1, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D2, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D3, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D4, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D5, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D6, BCM2835_GPIO_FSEL_INPT);
-    bcm2835_gpio_fsel(D7, BCM2835_GPIO_FSEL_INPT);
+    // SEL0 pins 0-7 input. 8-9 output
+    // pin rr  9  8  7  6  5  4  3  2  1  0
+    //     00001001000000000000000000000000
+    //        0   9   0   0   0   0   0   0
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL0/4, 0x09000000UL);
+    // SEL1 pins 12-18 input. 10,11 output
+    // pin rr 19 18 17 16 15 14 13 12 11 10
+    //     00000000000000000000000000001001
+    //        0   8   0   0   0   0   0   9
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL1/4, 0x00000009UL);    
+    // SEL2 pins 22,23,27 input. 24,25 output       
+    // pin rr 29 28 27 26 25 24 23 22 21 20
+    //     00000000000000001001000000000000
+    //        0   0   0   0   9   0   0   0
+    bcm2835_peri_write_nb (gpio_base + BCM2835_GPFSEL2/4, 0x00009000UL);    
 }
         
 void c011_init(void) {
     bcm2835_init();
     set_control_output_pins();
     bcm2835_gpio_write(ANALYSE, LOW);
+    gpio_base = bcm2835_regbase(BCM2835_REGBASE_GPIO);
 }
 
 void c011_reset(void) {
@@ -79,6 +99,8 @@ void c011_analyse(void) {
 }
 
 void c011_enable_out_int(void) {
+    abort ();   //TODO int not used
+    /*
     set_data_output_pins();
     bcm2835_gpio_write_mask (1<<RS1 | 1<<RS0 | 0<<RW | 1<<CS,
                              1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS);
@@ -89,9 +111,12 @@ void c011_enable_out_int(void) {
     sleep_ns (TCSLCSH);
     bcm2835_gpio_write(CS, HIGH);
     sleep_ns (TCSHCSL);
+    */
 }
 
 void c011_enable_in_int(void) {
+    abort ();
+    /*    
     set_data_output_pins();
     bcm2835_gpio_write_mask (1<<RS1 | 0<<RS0 | 0<<RW | 1<<CS,
                              1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS);
@@ -101,13 +126,21 @@ void c011_enable_in_int(void) {
     sleep_ns (TCSLCSH);
     bcm2835_gpio_write(CS, HIGH);
     sleep_ns (TCSHCSL);
+    */
 }
 
+//timeout is ms
 int c011_write_byte(uint8_t byte, uint32_t timeout) {
+    timeout *= 1000;    //make us
     //wait for output ready
-    while ((c011_read_output_status() & 0x01) != 0x01 && timeout>0) {
-        bcm2835_delayMicroseconds(1000);
+    //optimised read output status (don't set the control regs each time)
+    bcm2835_gpio_write_mask (1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS,
+                             1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS);
+    uint8_t stat = read_c011();
+    while ((stat & 0x01) != 0x01 && timeout>0) {
+        bcm2835_delayMicroseconds(1);
         timeout--;
+        stat = read_c011();
     }
     if (timeout == 0) {
         return -1;
@@ -120,14 +153,14 @@ int c011_write_byte(uint8_t byte, uint32_t timeout) {
                              1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS);
     //D0-D7
     uint8_t d7,d6,d5,d4,d3,d2,d1,d0;
-    d7=(byte&0x80) >> 7;
-    d6=(byte&0x40) >> 6;
-    d5=(byte&0x20) >> 5;
-    d4=(byte&0x10) >> 4;
-    d3=(byte&0x08) >> 3;
-    d2=(byte&0x04) >> 2;
-    d1=(byte&0x02) >> 1;
-    d0=(byte&0x01) >> 0;
+    d7=(byte&0x80) != 0;
+    d6=(byte&0x40) != 0;
+    d5=(byte&0x20) != 0;
+    d4=(byte&0x10) != 0;
+    d3=(byte&0x08) != 0;
+    d2=(byte&0x04) != 0;
+    d1=(byte&0x02) != 0;
+    d0=(byte&0x01) != 0;
     bcm2835_gpio_write_mask (d7<<D7 | d6<<D6 | d5<<D5 | d4<<D4 | d3<<D3 | d2<<D2 | d1<<D1 | d0<<D0,
                              1<<D7 | 1<<D6 | 1<<D5 | 1<<D4 | 1<<D3 | 1<<D2 | 1<<D1 | 1<<D0);
     //CS=0
@@ -145,7 +178,7 @@ static uint8_t read_c011(void) {
     bcm2835_gpio_write(CS, LOW);
     //must allow time for data valid after !CS
     sleep_ns (TCSLDrV);
-    uint32_t reg = bcm2835_peri_read (bcm2835_regbase(BCM2835_REGBASE_GPIO) + BCM2835_GPLEV0/4);
+    uint32_t reg = bcm2835_peri_read (gpio_base + BCM2835_GPLEV0/4);
     d7=(reg & 1<<D7) != 0;
     d6=(reg & 1<<D6) != 0;
     d5=(reg & 1<<D5) != 0;
@@ -190,10 +223,17 @@ uint8_t c011_read_output_status(void) {
     return byte;
 }
 
+//timeout is ms
 int c011_read_byte(uint8_t *byte, uint32_t timeout) {
-    while ((c011_read_input_status() & 0x01) == 0x00 && timeout>0) {
-        bcm2835_delayMicroseconds(1000);
+    timeout *= 1000;    //make us
+    //optimised read input status (don't set the control regs each time)
+    bcm2835_gpio_write_mask (1<<RS1 | 0<<RS0 | 1<<RW | 1<<CS,
+                             1<<RS1 | 1<<RS0 | 1<<RW | 1<<CS);
+    uint8_t stat = read_c011();
+    while ((stat & 0x01) == 0x00 && timeout>0) {
+        bcm2835_delayMicroseconds(1);
         timeout--;
+        stat = read_c011();
     }
     if (timeout == 0) {
         return -1;
