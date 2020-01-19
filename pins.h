e8e8e53b4797a9ab743ba1225fb7df5447b319f5
James Wilson <macihenroomfiddling@gmail.com>
2020-01-19 17:45:46 +0000

    Add note on BRCM GPIO pin numbering

--- pins.h
+++ pins.h
@@ -5,21 +5,21 @@ bcm2835 lib uses 40 pin J8 connector pin numbering
 
 #include <bcm2835.h>
 
-#define D0 RPI_V2_GPIO_P1_03    //BLACK
-#define D1 RPI_V2_GPIO_P1_05    //BROWN
-#define D2 RPI_V2_GPIO_P1_07    //RED
-#define D3 RPI_V2_GPIO_P1_11    //ORANGE
-#define D4 RPI_V2_GPIO_P1_12    //YELLOW
-#define D5 RPI_V2_GPIO_P1_13    //GREEN
-#define D6 RPI_V2_GPIO_P1_15    //BLUE
-#define D7 RPI_V2_GPIO_P1_16    //VIOLET
+#define D0 RPI_V2_GPIO_P1_03    //BLACK     (GPIO pin 2)
+#define D1 RPI_V2_GPIO_P1_05    //BROWN     (GPIO pin 3)
+#define D2 RPI_V2_GPIO_P1_07    //RED       (GPIO pin 4)
+#define D3 RPI_V2_GPIO_P1_11    //ORANGE    (GPIO pin 17)
+#define D4 RPI_V2_GPIO_P1_12    //YELLOW    (GPIO pin 18)
+#define D5 RPI_V2_GPIO_P1_13    //GREEN     (GPIO pin 27)
+#define D6 RPI_V2_GPIO_P1_15    //BLUE      (GPIO pin 22)
+#define D7 RPI_V2_GPIO_P1_16    //VIOLET    (GPIO pin 23)
 
-#define RS0 RPI_V2_GPIO_P1_18   //GREY
-#define RS1 RPI_V2_GPIO_P1_19   //WHITE
-#define RESET RPI_V2_GPIO_P1_21 //YELLOW
-#define CS RPI_V2_GPIO_P1_22    //GREEN
-#define RW RPI_V2_GPIO_P1_23    //BLUE
-#define ANALYSE RPI_V2_GPIO_P1_24 //VIOLET
+#define RS0 RPI_V2_GPIO_P1_18   //GREY      (GPIO pin 24)
+#define RS1 RPI_V2_GPIO_P1_19   //WHITE     (GPIO pin 10)
+#define RESET RPI_V2_GPIO_P1_21 //YELLOW    (GPIO pin 9)
+#define CS RPI_V2_GPIO_P1_22    //GREEN     (GPIO pin 25)
+#define RW RPI_V2_GPIO_P1_23    //BLUE      (GPIO pin 11)
+#define ANALYSE RPI_V2_GPIO_P1_24 //VIOLET  (GPIO pin 8)
 
 
 
